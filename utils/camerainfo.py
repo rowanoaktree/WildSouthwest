82c8fb55f2f15af60e40cff15be55044c5e29edc
Rowan Converse <54452132+rowanoaktree@users.noreply.github.com>
2022-10-12 13:29:38 -0600

    Camera Info script

---
+++ utils/camerainfo.py
@@ -0,0 +1,39 @@
+#Rowan Converse
+#10/12/2022
+#Pull camera info from Zooniverse
+#This script requires two inputs: the subject export and the COCO format annotations pulled from Zooniverse (see Drones4Ducks /utils/Zooniverse2COCO for example)
+
+import json 
+import pandas as pd
+
+#Load data
+annotations = r"" #path to downloaded csv classification export from Zooniverse
+subjects = r"" #path to downloaded csv subject export from Zooniverse
+zooniverse = pd.read_csv(annotations)
+subjectinfo = pd.read_csv(subjects)
+
+
+camanno = []
+set_id = None
+subjs = {}
+cams = {}
+
+for y in range(len(subjectinfo)):
+    setsub = subjectinfo["subject_id"][y]
+    set = subjectinfo["subject_set_id"][y]
+    if setsub not in subjs:
+        subjs[setsub] = len(subjs) + 1
+        subject_id = subjs[setsub]
+        cams[set] = len(cams) + 1
+        camera_id = cams[set]
+    camera_annotation = {
+        'annotation_id': len(camanno)+1,
+        'image_id': setsub,
+        'camera': set,
+      }
+    camanno.append(camera_annotation)
+
+df1 = pd.DataFrame(zooniverse)
+df2 = pd.DataFrame(camanno)
+
+new3 = pd.merge(df1, df2, how='left', on=["image_id"])
